0a2f16eb6495b9678a46060555550360ef44fc21
JohnsonSentai <89706019+JohnsonSentai@users.noreply.github.com>
2024-07-16 11:03:44 -0500

    Level Collision
    I'm implementing level boundaries and collision for the car. also tried to implement the sprite flipping with this code 	if direction > 0:
    		Sprite2D.flip_h = false
    	elif direction < 0:
    		Sprite2D.flip_h = true
    also made the player a very small bit faster to match whit the first level being bigger than it

--- PlayerCar.gd
+++ PlayerCar.gd
@@ -1,17 +1,20 @@
 extends CharacterBody2D
 
-var speed = 200
+var speed = 260
 
 func _ready():
 	print("PlayerCar loaded")
 
 func _physics_process(_delta):
 	var input_vector = Vector2.ZERO
+
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	
+		
 	velocity = input_vector.normalized() * speed
 	move_and_slide()
+
 	
 #declaring score variable for scoring functionality	
 var score = 0
